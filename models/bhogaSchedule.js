7e16577b536672cb4dffc3c69e142341274bc563
sumit kumar <sumit.kumar.kushwaha@zs.com>
2025-09-22 00:14:42 +0530

    add

--- models/bhogaSchedule.js
+++ models/bhogaSchedule.js
@@ -5,32 +5,26 @@ const BhogaScheduleSchema = new mongoose.Schema(
     monday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     tuesday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     wednesday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     thursday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     friday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     saturday: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     sunday: {
       type: String,
